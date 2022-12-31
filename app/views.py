5d417c91d0608066b72dba2d7b4f7e4993819738
unknown <you@example.com>
2022-12-31 11:06:03 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def arundathi(request):
+    return render(request,'arundathi.html')
+
+def pasupathi(request):
+    return render(request,'pasupathi.html')
